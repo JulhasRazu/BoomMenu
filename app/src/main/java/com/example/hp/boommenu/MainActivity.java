5e7a3cd183ff19f693c5fa9dc1fa688d8817e21f
hp <razu3666@gmail.com>
2018-03-15 23:04:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hp/boommenu/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hp.boommenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
+
+import static com.nightonke.boommenu.Piece.PiecePlaceEnum.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    BoomMenuButton bmb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bmb = findViewById(R.id.boom);
+        bmb.setButtonEnum(ButtonEnum.TextOutsideCircle);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_5_1);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_5_1);
+
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
+                    .normalImageRes(R.drawable.user)
+                    .normalText("Butter Doesn't fly!");
+            bmb.addBuilder(builder);
+        }
+    }
+}
